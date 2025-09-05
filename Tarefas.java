f2c0eb6bc7bc094a24165faaf1d0b8e52b649540
Joao-Paulo2007 <jp20022007@gmail.com>
2025-09-04 21:06:13 -0300

    Agendador de Tarefa

---
+++ Tarefas.java
@@ -0,0 +1,23 @@
+public class Tarefas {
+private String descricao;
+private boolean concluida;
+public Tarefas(String descricao) {
+this.descricao = descricao;
+this.concluida = false; 
+}
+
+public String getDescricao() {
+return descricao;
+}
+public boolean isConcluida() {
+return concluida;
+}
+
+public void marcarComoConcluida() {
+this.concluida = true;
+}
+@Override
+public String toString() {
+return (concluida ? "[X]": "[ ]" ) + descricao;
+}
+}
